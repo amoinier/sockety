f127eb56a4a58a2ba0453126763ca61c035f74d6
Moinier Alex <amoinier@outlook.fr>
2020-03-09 19:00:27 +0100

    add not found + fix dockerFIle

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,7 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.png')))
 app.use('/sockety', sockety)
 
 app.use((_: express.Request, res: express.Response): void => {
-  res.status(404).send()
+  res.status(404).send('not found')
 })
 
 app.listen(process.env.HTTP_PORT || 3000, (): void => {
